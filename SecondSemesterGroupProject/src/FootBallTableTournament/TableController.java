e32347f75236f0158a43592ac15966aa2f75e9be
MANNUCCIWESTH\gabriele <gab.mannu@hotmail.it>
2017-04-20 23:43:05 +0200

    organized and Table changes

--- SecondSemesterGroupProject/src/FootBallTableTournament/TableController.java
+++ SecondSemesterGroupProject/src/FootBallTableTournament/TableController.java
@@ -6,100 +6,77 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class TableController {
-    Main scene = new Main();
-
-    @FXML
-    void UpdateTableContentPlayer(ActionEvent event) {
-
-    }
-
-    @FXML
-    void ShowSchedule(ActionEvent event) {
-    }
-
-
-    @FXML
-    void DeleteTableContentPlayer(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SavePlayer1(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SavePlayer2(ActionEvent event) {
-
-    }
-
-    @FXML
-    void UpdateTableContentMatch(ActionEvent event) {
-
-    }
-
-    @FXML
-    void DeleteTableContentMatch(ActionEvent event) {
-
-    }
-
-    @FXML
-    private Button ShowShedulBtt;
-
-    @FXML
-    private TableView<?> Table;
-
-    @FXML
-    private TableColumn<?, ?> Email;
 
     @FXML
     private TextField EmailP2;
-
-    @FXML
-    private TableColumn<?, ?> Rank;
-
     @FXML
     private TextField EmailP1;
-
-    @FXML
-    private Button FinalSave;
-
     @FXML
     private TextField NameP2;
-
-    @FXML
-    private TableColumn<?, ?> Name;
-
     @FXML
     private TextField NameP1;
-
-    @FXML
-    private TableColumn<?, ?> DOB;
-
-    @FXML
-    private TableColumn<?, ?> Team;
-
     @FXML
     private TextField DOBp2;
-
-    @FXML
-    private Button SavePlayer1;
-
     @FXML
     private TextField DOBp1;
 
     @FXML
-    private Button SavePlayer2;
+    private Button ShowShedulBtt;
     @FXML
-    void DispaySchedule (ActionEvent event) {
-    }
+    private Button FinalSave;
+    @FXML
+    private Button SavePlayer1;
+    @FXML
+    private Button SavePlayer2;
 
     @FXML
-    void CommitToDB(ActionEvent event) {
+    private TableView<TableController> Table;
+
+    @FXML
+    private TableColumn<TableController, String> Name;
+    @FXML
+    private TableColumn<TableController, String> Email;
+    @FXML
+    private TableColumn<TableController, String> DOB;
+    @FXML
+    private TableColumn<TableController, String> Rank;
+    @FXML
+    private TableColumn<TableController, String> Team;
+
+        @FXML
+        void LoadPlayerOnTable(ActionEvent event) {
+
+            }
+            @FXML
+        void CommitToDB(ActionEvent event) {
 
         }
-    }
+
+        @FXML
+        void DeleteTableContentPlayer(ActionEvent event) {
+
+        }
+
+
+
+        @FXML
+        void UpdateTableContentMatch(ActionEvent event) {
+
+        }
+
+        @FXML
+        void DeleteTableContentMatch(ActionEvent event) {
+
+        }
+
+        @FXML
+        void DispaySchedule(ActionEvent event) {
+
+        }
+        }
