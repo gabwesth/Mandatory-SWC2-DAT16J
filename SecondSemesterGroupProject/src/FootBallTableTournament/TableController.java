1d5a70f02f2f07f299422811a133029c4a392b6c
MANNUCCIWESTH\gabriele <gab.mannu@hotmail.it>
2017-04-20 14:18:41 +0200

    Table Controller Fields

--- SecondSemesterGroupProject/src/FootBallTableTournament/TableController.java
+++ SecondSemesterGroupProject/src/FootBallTableTournament/TableController.java
@@ -1,19 +1,55 @@
 package FootBallTableTournament;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class TableController {
 
     @FXML
-    void UpdateTableContentPlayer(ActionEvent event) {
+    private TextField NameP1;
+
+    @FXML
+    private TextField NameP2;
+
+    @FXML
+    private TextField EmailP1;
+
+    @FXML
+    private TextField EmailP2;
+
+    @FXML
+    private TextField DOBp1;
+
+    @FXML
+    private TextField DOBp2;
+
+    @FXML
+    private Button SavePlayer1;
+
+    @FXML
+    private Button SavePlayer2;
+
+    @FXML
+    private Button ShowShedulBtt;
+
+    @FXML
+    private Button FinalSave;
+
+
+
+
+    @FXML
+    void LoadPlayerOnTable(ActionEvent event) {
+
+
+
+    }
+
+    @FXML
+    void CommitToDB(ActionEvent event) {
 
     }
 
@@ -22,15 +58,7 @@ public class TableController {
 
     }
 
-    @FXML
-    void SavePlayer1(ActionEvent event) {
 
-    }
-
-    @FXML
-    void SavePlayer2(ActionEvent event) {
-
-    }
 
     @FXML
     void UpdateTableContentMatch(ActionEvent event) {
@@ -41,4 +69,10 @@ public class TableController {
     void DeleteTableContentMatch(ActionEvent event) {
 
     }
+
+    @FXML
+    void DispaySchedule(ActionEvent event) {
+
+    }
+
 }
